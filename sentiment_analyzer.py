b0cea6cc033cd868c2d4e14de534baeeb188f970
Chen Yao <cyaom16@gmail.com>
2018-04-08 16:25:57 -0400

    Add PMI calculation and update docstring

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -113,8 +113,8 @@ class SentimentAnalyzer:
         Tokenize text by the specified type
 
         Args:
-            text: Input text
-            term_type: word or noun phrase, e.g., 'w' or 'np'
+            text:      str. Input text
+            term_type: str. Word or noun phrase, e.g., 'w' or 'np'
 
         Returns:
             tokens: A list of tokens
@@ -133,14 +133,14 @@ class SentimentAnalyzer:
 
         return tokens
 
-    def get_topk_terms(self, k, label_val, term_type='w'):
+    def get_topk_terms(self, k, label, term_type='w'):
         """
         Retrieve top-k terms (e.g., words or noun phrases).
 
         Args:
-            k:         Top-k
-            label_val: Either 'Positive' or 'Negative'
-            term_type: word or noun phrase, e.g., 'w' or 'np'
+            k:         int. Top-k
+            label:     str. Label, e.g., 'positive' or 'negative'
+            term_type: str. Word or noun phrase, e.g., 'w' or 'np'
 
         Returns:
             Top-k frequent terms (A list of tuples)
@@ -148,76 +148,109 @@ class SentimentAnalyzer:
 
         counter = Counter()
         # Retrieve reviews labeled by the specified label_val
-        label_reviews = self.df.loc[self.df[self.truth_col] == label_val][self.review_col]
+        label_reviews = self.df.loc[self.df[self.truth_col] == label][self.review_col]
         for review in label_reviews:
-            terms = self.tokenize(review, term_type=term_type)
+            terms = self.tokenize(review, term_type)
             counter.update(terms)
 
         return counter.most_common(k)
 
-    def get_coocurr(self, k, label_val, term_type='w', **kwargs):
+    def get_coocurr(self, k, label, term_type='w', **kwargs):
         """
-        Calculate co-occurrences of terms specified with reviews
+        Calculate term co-occurrences w.r.t reviews
 
         Args:
-            k:         Top-k
-            label_val: 'Positive' or 'Negative'
-            term_type: word or noun phrase, e.g., 'w' or 'np'
+            k:         int. Top-k
+            label:     str. Label, e.g., 'positive' or 'negative'
+            term_type: str. Word or noun phrase, e.g., 'w' or 'np'
+
+            kwargs:
+                topk: list.
 
         Returns:
             A DataFrame with co-occurrences of terms
         """
 
-        topk = kwargs.get('topk', self.get_topk_terms(k, label_val, term_type))
+        topk = kwargs.get('topk', self.get_topk_terms(k, label, term_type))
         topk_terms = [term for term, _ in topk]
 
         coocurr = []
         for review in self.df[self.review_col]:
-            counter = Counter(self.tokenize(review, term_type))
-            coocurr.append([1 if counter[term] else 0 for term in topk_terms])
+            tokens = set(self.tokenize(review, term_type))
+            coocurr.append([1 if term in tokens else 0 for term in topk_terms])
 
         coocurr_df = pd.DataFrame(coocurr)
         coocurr_df.column = topk_terms
 
         return coocurr_df
 
-    def get_mi(self, k, label_val, term_type='w', **kwargs):
+    def get_mi(self, k, label, term_type='w', **kwargs):
         """
-        Calculate mutual information score for each term
+        Calculate mutual information score for each term w.r.t ground truths
 
         Args:
-            k:         Top-k
-            label_val: Either 'Positive' or 'Negative'
-            term_type: word or noun phrase, e.g., 'w' or 'np'
+            k:         int. Top-k
+            label:     str. Label, e.g., 'positive' or 'Negative'
+            term_type: str. Word or noun phrase, e.g., 'w' or 'np'
+
+            kwargs:
+                topk:       list. A list of top-k word counts
+                coocurr_df: DataFrame.
 
         Returns:
-            mi_df: A DataFrame that contains mutual information score for each term
+            mi_df: A DataFrame that contains MI score for each term
         """
 
-        topk = kwargs.get('topk', self.get_topk_terms(k, label_val, term_type))
+        topk = kwargs.get('topk', self.get_topk_terms(k, label, term_type))
         topk_terms = [term for term, _ in topk]
 
-        coocurr_df = kwargs.get('coocurr', self.get_coocurr(k, label_val, term_type, topk=topk))
+        coocurr_df = kwargs.get('coocurr', self.get_coocurr(k, label, term_type, topk=topk))
 
         labels_true = self.df[self.truth_col]
-        mi_scores = [metrics.mutual_info_score(labels_true, coocurr_df[term]) for term in topk_terms]
-        mi_df = pd.DataFrame(mi_scores)
-        mi_df.columns = topk_terms
+
+        mi = [[term, metrics.mutual_info_score(labels_true, coocurr_df[term])]
+              for term in topk_terms]
+
+        mi_df = pd.DataFrame(mi)
+        mi_df.columns = ['Term', 'MI']
 
         return mi_df
 
-    def get_pmi(self):
+    def get_pmi(self, k, label, term_type='w', **kwargs):
         """
-        Calculate point-wise mutual information score for each term
+        Calculate point-wise mutual information score for each term w.r.t the input label
 
         Args:
+            k:         int. Top-k
+            label:     str. Label, e.g., 'positive' or 'Negative'
+            term_type: str. Word or noun phrase, e.g., 'w' or 'np'
 
-        :return:
+            kwargs:
+                topk:       list. A list of top-k word counts
+                coocurr_df: DataFrame.
+
+        Returns:
+            pmi_df: A DataFrame that contains PMI score for each term
+
         """
-        # df = dff.copy()
-        # df['f_x'] = df.groupby(x)[x].transform('count')
-        # df['f_y'] = df.groupby(y)[y].transform('count')
-        # df['f_xy'] = df.groupby([x, y])[x].transform('count')
-        # df['pmi'] = math.log(len(df.index) * df['f_xy'] / (df['f_x'] * df['f_y']))
-        # return df
 
+        topk = kwargs.get('topk', self.get_topk_terms(k, label, term_type))
+        topk_terms = [term for term, _ in topk]
+
+        coocurr_df = kwargs.get('coocurr', self.get_coocurr(k, label, term_type, topk=topk))
+        num = coocurr_df.shape[0]
+        pmi = []
+        epsilon = 1e-4
+        for term in topk_terms:
+            px = coocurr_df[term].sum()
+            py = coocurr_df[self.truth_col == label].sum()
+            pxy = len(coocurr_df[(coocurr_df[self.truth_col] == label) & (coocurr_df[term] == 1)])
+
+            score = math.log2(num * (pxy + epsilon) / (px * py))
+
+            pmi.append([term, score])
+
+        pmi_df = pd.DataFrame(pmi)
+        pmi_df.columns = ['Term', 'PMI']
+
+        return pmi_df
